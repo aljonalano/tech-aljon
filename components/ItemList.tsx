fcc1a39cb2efe30be0a27cbf08c3b25939851690
Aljon <alano.aljonvincent@gmail.com>
2024-08-05 17:36:46 +0800

    Fix: Edit items components

--- components/ItemList.tsx
+++ components/ItemList.tsx
@@ -38,38 +38,40 @@ export default async function ItemList() {
         <>
           <div className="flex">
             <Sidebar />
-            <div className='className="flex-1 p-[6.5rem]'>
-              {items?.map((i: any) => (
-                <Link key={i?.id} href={`/edit/${i?.id}`} legacyBehavior>
-                  <div className="bg-white shadow-md rounded-lg h-60 border border-black flex mb-4 relative cursor-pointer">
-                    <Image
-                      src="/curry.png"
-                      alt="Dish"
-                      className="w-[20rem] rounded-lg object-cover mr-4"
-                      width={100}
-                      height={100}
-                    />
+            <div className="flex-1 p-[6.5rem]">
+              {items?.map((i: any) =>
+                i ? (
+                  <Link key={i.id} href={`/edit-items/${i.id}`} legacyBehavior>
+                    <div className="bg-white shadow-md rounded-lg h-60 border border-black flex mb-4 relative cursor-pointer">
+                      <Image
+                        src="/curry.png"
+                        alt="Dish"
+                        className="w-[20rem] rounded-lg object-cover mr-4"
+                        width={100}
+                        height={100}
+                      />
 
-                    <div className="flex-1 p-4">
-                      <h3 className="text-[30px] font-semibold text-[#000000]">
-                        {i?.title}
-                      </h3>
-                      <p className="text-[#000000] font-semibold">
-                        {i?.description}
-                      </p>
-                      <a href="#" className="text-[#000000] text-[13px]">
-                        See more
-                      </a>
-                      <div className="flex justify-between mt-[13px] font-semibold text-[#000000]">
-                        <p className="text-sm ">Added by: {i?.addedBy}</p>
-                        <p className="text-sm">
-                          Date: {formatDate(i?.createdAt)}
+                      <div className="flex-1 p-4">
+                        <h3 className="text-[30px] font-semibold text-[#000000]">
+                          {i.title}
+                        </h3>
+                        <p className="text-[#000000] font-semibold">
+                          {i.description}
                         </p>
+                        <a href="#" className="text-[#000000] text-[13px]">
+                          See more
+                        </a>
+                        <div className="flex justify-between mt-[13px] font-semibold text-[#000000]">
+                          <p className="text-sm">Added by: {i.addedBy}</p>
+                          <p className="text-sm">
+                            Date: {formatDate(i.createdAt)}
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              ))}
+                  </Link>
+                ) : null
+              )}
             </div>
           </div>
           {/* <hr className="bg-[#213b9b] border-2" /> */}
